7d09b3536e1421b2e5a4cd6b78124f7edf3d8b2f
Alaa Gaafer <alaa.u.gaafer@gmail.com>
2023-12-04 03:43:12 +0200

    Updated scripts

--- backend/autoAnalysisServer/preprocessing_Scripts/functions.py
+++ backend/autoAnalysisServer/preprocessing_Scripts/functions.py
@@ -6,8 +6,23 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 
 class Convert_to_datetime:
+    """
+    A class for converting object-type columns to datetime in a pandas DataFrame.
+
+    Methods:
+        - convert(df): Converts object-type columns to datetime.
+    """
 
     def convert(self, df):
+        """
+        Convert object-type columns to datetime.
+
+        Parameters:
+            - df: pandas DataFrame
+
+        Returns:
+            - df: pandas DataFrame with converted datetime columns
+        """
 
         for col in df.columns:
             if pd.api.types.is_object_dtype(df[col]):
@@ -20,11 +35,50 @@ class Convert_to_datetime:
 
 
 class MissingValues:
+    """
+    A class for handling missing values in a pandas DataFrame.
 
-    def handle_nan(self, df, handle='auto'):
-        # Function for handling missing values in the data
-        if handle == 'auto':
-            for col in df.columns:
+    Methods:
+        - handle_nan(df, handle='auto'): Handles missing values based on the specified method.
+        - fill_datetime_na(series): Fills missing values in a datetime series with sequential dates if it is sequential.
+    """
+
+    def detect_nulls(self, df):
+        """
+        Detects null values in each column and informs the user.
+
+        Parameters:
+            - df: pandas DataFrame
+
+        Returns:
+            - None
+        """
+        null_info = df.isnull().sum()
+        columns_with_nulls = null_info[null_info > 0]
+
+        if columns_with_nulls.empty:
+            print("No null values detected.")
+        else:
+            print("Null values detected:")
+            for col, count in columns_with_nulls.items():
+                print(f"Column: {col}, Number of Nulls: {count}")
+                print(f"Locations of Nulls: {df.index[df[col].isnull()].tolist()}")
+                print("\n")
+        return columns_with_nulls
+
+    def handle_nan(self, df, fillNA_dict):
+        """
+        Handles missing values in the data.
+
+        Parameters:
+            - df: pandas DataFrame
+            - handle: 'auto' or 'delete'
+
+        Returns:
+            - df: pandas DataFrame with missing values handled
+        """
+        for col, method in fillNA_dict.items():
+            if method == 'auto':
                 if not df[col].isnull().any():
                     continue
                 if is_float_dtype(df[col]):
@@ -35,59 +89,216 @@ class MissingValues:
                     df[col], status = self.fill_datetime_na(df[col])
                     if status == "failed":
                         df[col].dropna(inplace=True)
-
-        elif handle == 'delete':
-            df.dropna(inplace=True)
+            elif method == 'median':
+                if is_float_dtype(df[col]) or is_integer_dtype(df[col]):
+                    df[col].fillna(df[col].median(), inplace=True)
+            elif method == 'mode':
+                df[col].fillna(df[col].mode()[0], inplace=True)
+            elif method == 'mean':
+                if is_float_dtype(df[col]) or is_integer_dtype(df[col]):
+                    df[col].fillna(df[col].mean()[0], inplace=True)
+            elif method == 'delete':
+                df.dropna(subset = [col], inplace=True)
         return df
 
     def fill_datetime_na(self, series):
-        # Fill missing values in a datetime series with sequential dates if it is sequential
-        date_diffs = series.diff()
+        """
+        Fills missing values in a datetime series based on the dynamically calculated pattern of dates.
 
-        if date_diffs.min() == date_diffs.max() == pd.Timedelta(days=1):
-            start_date = series.min()
-            end_date = series.max()
-            date_range = pd.date_range(start_date, end_date, freq='D')
-            series_filled = series.combine_first(pd.Series(date_range))
-            print("Nan locations: ", series.index[series.isnull()])
-            status = "success"
-        else:
-            # If the datetime values are not sequential, drop the row
-            status = "failed"
+        Parameters:
+            - series: pandas Series
+                The input datetime series with missing values to be filled.
 
-        return series_filled, status
+        Returns:
+            - filled_series: pandas Series
+                The datetime series with filled missing values based on the dynamically calculated pattern.
+            - status: str
+                The status of the filling operation, either 'success' or 'failed'.
+        """
+        series_copy = series.copy()
+        print("Nan locations: ", series_copy.index[series_copy.isnull()])
+        status = "failed"
+        old_index = series_copy.index
+        series_copy = series_copy.reindex(range(old_index.min(), old_index.max() + 1))
+        added_indices = series_copy.index.difference(old_index).tolist()
+        print("added_indices",added_indices)
+
+        if series_copy.isnull().any():
+            max_sequential_non_nulls = 4
+            max_count = 0
+            count = 0
+            start_index = 0
+
+            # Find the index where the sequential non-null dates start
+            for i, date in enumerate(series_copy):
+                if pd.notnull(date):
+                    count += 1
+                    if count > max_count:
+                        max_count = count
+                        start_index = i - count + 1
+                else:
+                    count = 0
+
+            # Check if the max number of sequential non-null dates is sufficient
+            if max_count >= max_sequential_non_nulls:
+                prev_gap = (series_copy[start_index + 1] - series_copy[start_index]).total_seconds() / (
+                        60 * 60 * 24)  # Convert to days
+                c = 0
+
+                # Check if the gaps between sequential non-null dates are consistent
+                for i in range(start_index + 1, max_sequential_non_nulls + start_index - 1):
+                    gap = (series_copy[i + 1] - series_copy[i]).total_seconds() / (60 * 60 * 24)  # Convert to days
+                    if gap == prev_gap:
+                        c += 1
+
+                dynamic_pattern = (series_copy[start_index + 1] - series_copy[start_index]).total_seconds() / (60 * 60 * 24)
+
+                # If the gaps are consistent, fill NaN values using the dynamic pattern
+                if c == (max_sequential_non_nulls - 2):
+                    dynamic_pattern = (series_copy[start_index + 1] - series_copy[start_index]).total_seconds() / (60 * 60 * 24)
+                    print("Gap Pattern = ", dynamic_pattern)
+
+                    # Fill NaN values before the sequence
+                    for i in range(start_index - 1, -1, -1):
+                        series_copy.loc[i] = series_copy.loc[i + 1] - pd.to_timedelta(dynamic_pattern, unit='D')
+
+                    # Fill NaN values after the sequence
+                    for i in range(start_index + max_sequential_non_nulls, len(series_copy)):
+                        series_copy.loc[i] = series_copy.loc[i - 1] + pd.to_timedelta(dynamic_pattern, unit='D')
+
+                    status = "success"
+                    series_copy = series_copy.drop(added_indices)
+
+                else:
+                    print("Failed to detect a pattern. All nulls in the date column were dropped")
+                    status = "failed"
+
+            else:
+                # If the max number of sequential non-null dates is not sufficient, drop the row
+                print("The Date column has too many nulls. Maybe you want to refill it manually and re-upload the data")
+                print("All nulls in the date column were dropped")
+                status = "failed"
+
+        return series_copy, status
 
 
 class Duplicates:
+    """
+       A class for handling duplicate values in a pandas DataFrame.
 
-    def handle_dub(self, df, method="auto"):
-        # Function for handling Duplications values in the data
-        if method == "auto":
-            for col in df.columns:
-                df.drop_duplicates(inplace=True)
+       Methods:
+       - handle_dub(df, method='auto'): Handles duplicate values based on the specified method.
+    """
+
+    def handle_dub(self, df):
+        """
+        Handle duplicate values in the data.
+
+        Parameters:
+            - df: pandas DataFrame
+        Returns:
+            - df: pandas DataFrame with duplicate values handled
+        """
+        df.drop_duplicates(inplace=True)
         return df
 
 
 class Outliers:
+    """
+    A class for handling outliers in a pandas DataFrame.
 
-    def handle_outliers(self, df, method='z_score', handle='auto', threshold=3):
-        if method == 'z_score':
-            for col in df.select_dtypes(include=['float64']).columns:
+    Methods:
+        - handle_outliers(df, method='z_score', handle='auto', threshold=3): Handles outliers based on the specified method.
+    """
+
+    def detect_outliers(self, df, threshold=3):
+        """
+        Detect outliers in the data and inform the user.
+
+        Parameters:
+            - df: pandas DataFrame
+            - method: 'z_score' or specific method for detecting outliers
+            - threshold: Z-score threshold for identifying outliers
+
+        Returns:
+            - None
+        """
+        outlier_info = {}
+
+        for col in df.select_dtypes(include=['float64']).columns:
+            z_scores = np.abs((df[col] - df[col].mean()) / df[col].std())
+            outliers_mask = z_scores > threshold
+
+            if outliers_mask.any():
+                outlier_info[col] = {
+                    'locations': df.index[outliers_mask].tolist(),
+                    'message': 'Extreme outliers detected. Consider handling them.'
+                }
+            elif np.any((z_scores > 1) & (z_scores <= threshold)):
+                outlier_info[col] = {
+                    'locations': df.index[z_scores > 1].tolist(),
+                    'message': 'Mild outliers detected. Consider handling them.'
+                }
+
+
+        return outlier_info
+
+    def handle_outliers(self, df, methods):
+        """
+        Handle outliers in the data.
+
+        Parameters:
+            - df: pandas DataFrame
+            - methods: Dictionary with keys as column names, values as tuples (method, handle, threshold)
+                       where method can be 'z_score' or 'IQR', handle can be 'auto', 'delete', 'median', 'mean', and threshold is the outlier threshold.
+
+        Returns:
+            - df: pandas DataFrame with outliers handled
+        """
+        # in the website threshold will be a default of 1.5 in case of IQR and 3 in case of z-score if the user doesn't enter another one.
+        for col, (method, handle, threshold) in methods.items():
+            outliers_mask = None
+            if method == 'z_score':
                 z_scores = np.abs((df[col] - df[col].mean()) / df[col].std())
                 outliers_mask = z_scores > threshold
-                if handle == 'auto':
-                    df.loc[outliers_mask, col] = df[col].median()
-                elif handle == 'delete':
-                    deletion_mask = ~outliers_mask
-                    df = df[deletion_mask]
-                    print(df.head(61))
+            elif method == 'IQR':
+                Q1 = df[col].quantile(0.25)
+                Q3 = df[col].quantile(0.75)
+                IQR = Q3 - Q1
+                lower_bound = Q1 - threshold * IQR
+                upper_bound = Q3 + threshold * IQR
+                outliers_mask = (df[col] < lower_bound) | (df[col] > upper_bound)
+
+            if handle == 'auto' or handle == 'median':
+                df.loc[outliers_mask, col] = df[col].median()
+            elif handle == 'delete':
+                deletion_mask = ~outliers_mask
+                df = df[deletion_mask]
+            elif handle == 'mean':
+                df.loc[outliers_mask, col] = df[col].mean()
 
         return df
 
 
 class DataNormalization:
+    """
+        A class for normalizing numeric data in a pandas DataFrame.
+
+        Methods:
+        - normalize_data(df, method='standard'): Normalizes numeric data based on the specified method.
+    """
 
     def normalize_data(self, df, method='standard'):
+        """
+        Normalize numeric data in the DataFrame.
+
+        Parameters:
+            - df: pandas DataFrame
+            - method: 'standard' or specific method for normalization
+
+        Returns:
+            - df: pandas DataFrame with normalized numeric data
+        """
         if method == 'standard':
             scaler = StandardScaler()
             df[df.select_dtypes(include=['float64']).columns] = scaler.fit_transform(
@@ -96,18 +307,24 @@ class DataNormalization:
 
 
 class EncodeCategorical:
+    """
+        A class for encoding categorical columns in a pandas DataFrame.
+
+        Methods:
+        - Encode(df, encoding_dict): Encodes categorical columns based on the specified encoding method.
+    """
 
     def Encode(self, df, encoding_dict):
         """
-                Encode categorical columns in a DataFrame based on the specified encoding method wanted by the user.
+        Encode categorical columns in a DataFrame based on the specified encoding method wanted by the user.
 
-                Parameters:
-                - df: pandas DataFrame
-                - encoding_dict: a dictionary where keys are column names and values are encoding methods ("label" or "onehot")
+            Parameters:
+            - df: pandas DataFrame
+            - encoding_dict: a dictionary where keys are column names and values are encoding methods ("label" or "onehot")
 
-                Returns:
-                - df_encoded: pandas DataFrame with encoded categorical columns
-                """
+        Returns:
+            - df_encoded: pandas DataFrame with encoded categorical columns
+        """
 
         df_encoded = df.copy()
 
@@ -125,25 +342,59 @@ class EncodeCategorical:
 
 
 class HandlingColinearity:
-    def remove_low_variance(self, df_numeric, var_threshold=0.1):
-        columns_to_drop = []
+    """
+    A class for handling co-linearity in numeric columns of a pandas DataFrame.
 
-        for column in df_numeric.columns:
-            if df_numeric[column].var() < var_threshold:
-                columns_to_drop.append(column)
+    Methods:
+        - detect_low_variance(df_numeric, var_threshold=0.1): Detects and informs about columns with low variance.
+        - handle_low_variance(df, actions): Handles low variance based on user actions.
+        - detect_colinearity(df): Detects and informs about colinearity between numeric variables.
+        - handle_colinearity(df, actions): Handles colinearity based on user actions.
+    """
 
-        df_numeric.drop(columns=columns_to_drop, inplace=True)
-        return df_numeric
+    def detect_low_variance(self, df, var_threshold=0.1):
+        """
+        Detect and inform about columns with low variance in numeric columns.
+
+        Parameters:
+            - df_numeric: pandas DataFrame with numeric columns
+            - var_threshold: Variance threshold for detecting low variance
+
+        Returns:
+            - low_variance_columns: List of columns with low variance
+        """
+        df_numeric = df.select_dtypes(include=['number'])
+        low_variance_columns = [column for column in df_numeric.columns if df_numeric[column].var() < var_threshold]
+        if low_variance_columns:
+            print("The following columns have low variance:")
+            for col in low_variance_columns:
+                print(f"- {col}")
+        else:
+            print("No columns with low variance detected.")
+        return low_variance_columns
+
+    def handle_low_variance(self, df, actions):
+        """
+        Handle low variance in numeric columns based on user actions.
+
+        Parameters:
+            - df: pandas DataFrame
+            - actions: Dictionary with column names as keys and values 'keep', 'remove', or 'auto'
+
+        Returns:
+            - df: pandas DataFrame with low variance handled
+        """
+        for column, action in actions.items():
+            if action == 'remove' or action == 'auto' and column in df.columns:
+                df.drop(columns=column, inplace=True)
+        return df
 
     def handling_colinearity(self, df, auto_handling=True):
         # Select numeric variables only
         numeric_df = df.select_dtypes(include='number')
 
-        # Remove variables with low variance
-        new_df = self.remove_low_variance(numeric_df)
-
         # Build Correlation Matrix and choose the correlated variables
-        corr = round(new_df.corr(), 2)
+        corr = round(df.corr(), 2)
         high_corr_pairs = (corr.abs() > 0.5) & (corr != 1)
         high_corr_variables = corr[high_corr_pairs].stack()
         variables_cleaned = high_corr_variables.drop_duplicates()
