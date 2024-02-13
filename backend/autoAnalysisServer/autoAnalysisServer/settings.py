531ff4ebaa4ddeaca2b0bf67add3620734a5abcd
Assem Ihab <assimehab@gmail.com>
2024-02-13 19:53:30 +0200

    ignoring the cors policy for now

--- backend/autoAnalysisServer/autoAnalysisServer/settings.py
+++ backend/autoAnalysisServer/autoAnalysisServer/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'retTuner.apps.RettunerConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'autoAnalysisServer.urls'
@@ -80,6 +82,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Password validation
