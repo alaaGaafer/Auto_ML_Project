8f0ee0c4960ece8c3273cb376fc1550779c72aea
Assem Ihab <assimehab@gmail.com>
2024-06-30 12:41:06 +0300

    may return to this commit

--- backend/autoAnalysisServer/autoAnalysisServer/urls.py
+++ backend/autoAnalysisServer/autoAnalysisServer/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from preprocessing import views
+# from preprocessing import views
 
 
 urlpatterns = [
